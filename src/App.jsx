9c4fec246146903ecfa43289dda7dd8ec0bd7e9e
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-21 23:19:23 +0100

    Create aboutme and art page with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./assets/Components/Header";
-
+import Art from "./assets/Pages/Art";
+import AboutMe from "./assets/Pages/AboutMe";
+import Contact from "./assets/Pages/Contact";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <main className="bg-slate-900 text-white flex flex-col justify-center items-center  h-screen">
-        <h1 className="text-center">Hello World</h1>
-      </main>
-    </>
+    <Router>
+      <Header />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/Art">Art</Link>
+          </li>
+          <li>
+            <Link to="/AboutMe">About Me</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/Art" element={<Art />} />
+        <Route path="/AboutMe" element={<AboutMe />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
-
 
 export default App;
--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,23 +13,41 @@ const Header = () => {
   };
 
   return (
-    <div className="h-20 flex flex-col md:flex-row justify-between items-center">
-      <div className="flex flex-row items-center">
+    <div className="h-20 flex flex-row md:flex-row justify-between items-center">
+      <div className="flex  items-center">
         <h1 className="font-bold ml-5 text-xl">Josué Salazaku</h1>
       </div>
 
       {/* Hamburger Icon */}
       <div
-        className="cursor-pointer md:hidden flex flex-row space-x-2 ml-5"
+        className="cursor-pointer md:hidden flex flex-row space-x-2 mr-5"
         onClick={toggleMenu}
       >
-        <div className={`h-6 w-0.5 bg-black mb-1 transition-transform transform ${menuOpen ? 'rotate-45' : 'rotate-0'}`}></div>
-        <div className={`h-6 w-0.5 bg-black mb-1 transition-opacity ${menuOpen ? 'opacity-0' : 'opacity-100'}`}></div>
-        <div className={`h-6 w-0.5 bg-black transition-transform transform ${menuOpen ? '-rotate-45' : 'rotate-0'}`}></div>
+        <div
+          className={`h-6 w-0.5 bg-black mb-1 transition-transform transform ${
+            menuOpen ? "rotate-45" : "rotate-0"
+          }`}
+        ></div>
+        <div
+          className={`h-6 w-0.5 bg-black mb-1 transition-opacity ${
+            menuOpen ? "opacity-0" : "opacity-100"
+          }`}
+        ></div>
+        <div
+          className={`h-6 w-0.5 bg-black transition-transform transform ${
+            menuOpen ? "-rotate-45" : "rotate-0"
+          }`}
+        ></div>
       </div>
 
       {/* Navigation Items */}
-      <ul className={`md:flex space-x-5 ${menuOpen ? 'flex flex-col md:flex-row absolute md:static top-20 md:top-0 right-0 left-0 md:right-auto' : 'hidden md:flex'}`}>
+      <ul
+        className={`md:flex space-x-5 font-extrabold ${
+          menuOpen
+            ? "flex flex-col md:flex-row absolute md:static top-20 md:top-0 right-0 left-0 md:right-auto items-center"
+            : "hidden md:flex"
+        }`}
+      >
         <li className="my-3" onClick={closeMenu}>
           <a href="">Art</a>
         </li>
@@ -43,7 +62,7 @@ const Header = () => {
       {/* Navigation Overlay */}
       {menuOpen && (
         <div
-          className="fixed inset-0 bg-gray-800 bg-opacity-50 z-50"
+          className="fixed inset-0 bg-white bg-opacity-50 z-50"
           onClick={closeMenu}
         ></div>
       )}
