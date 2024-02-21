eda209e98b1025de942835ede9a5497b1519ebbb
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-21 21:09:42 +0100

    Add list for pages in header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./assets/Components/Header";
 
 
 function App() {
   return (
     <>
+      <Header/>
       <main className="bg-slate-900 text-white flex flex-col justify-center items-center  h-screen">
         <h1 className="text-center">Hello World</h1>
       </main>
--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -2,11 +2,12 @@
 
 const Header = () => {
   return (
-      <div>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+      <div className="h-20 flex flex-row justify-evenly items-center">
+          <h1 className="font-bold mr-20 text-4xl">LOGO</h1>
+          <ul className="flex flex-row justify-between space-x-5">
+            <li>Art</li>
+            <li>About me</li>
+            <li>Contact</li>
           </ul>
       </div>
   )
