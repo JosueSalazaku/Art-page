8dc9d2f799ef45f5bdb5f1b65f59c6044ea25855
Josué Salazaku <josuesalazaku@gmail.com>
2025-05-24 00:54:34 +0200

    Implement Navigation component and update layout structure

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Navigation() {
   return (
-    <div className='flex items-center h-16'>NAV</div>
+      <div className='flex items-center h-16'><ul>
+      <li><a href="/bio">Bio</a></li>
+      <li><a href="/art">art</a></li>
+      <li><a href="/contact">contact</a></li>
+      </ul></div>
   )
 }
